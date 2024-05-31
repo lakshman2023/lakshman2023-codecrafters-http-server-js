4864f916df4c52cf5e9ed7b9f8705358e76a8faa
lakshman2023 <lakshmanborra333@gmail.com>
2024-05-31 18:05:39 +0530

    pass 5th stage

--- app/main.js
+++ app/main.js
@@ -6,18 +6,27 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
-    const req = data.toString();
-    const reqPath = req.split(' ')[1];
-    if(reqPath === '/'){
-        const res = "HTTP/1.1 200 OK\r\n\r\n";
-        socket.write(res);
-    } else if(reqPath.startsWith('/echo/')){
-        const content = reqPath.split("echo/")[1];
-        const res = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`;
-        socket.write(res);
-    }else {
-        const res = "HTTP/1.1 404 Not Found\r\n\r\n";
-        socket.write(res);
+    try {
+        const req = data.toString();
+        const reqLines = req.split('\r\n');
+        const reqPath = req.split(' ')[1];
+        if(reqPath === '/'){
+            const res = "HTTP/1.1 200 OK\r\n\r\n";
+            socket.write(res);
+        } else if(reqPath.startsWith('/echo/')){
+            const content = reqPath.split("echo/")[1];
+            const res = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`;
+            socket.write(res);
+        }else if(reqPath.startsWith('/user-agent')) {
+            const userAgent = reqLines.find(e => e.includes('User-Agent')).split(': ')[1];
+            const res = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
+            socket.write(res);
+        }else {
+            const res = "HTTP/1.1 404 Not Found\r\n\r\n";
+            socket.write(res);
+        }
+    } catch (error) {
+        socket.write(`HTTP/1.1 500 Internal Server Error\r\n\r\n`);
     }
   });
   socket.on("close", () => {
